52ad319f3b19f82a907003a1a93759eddce7074f
Amalay <amalayverma@gmail.com>
2018-06-28 21:45:52 +0530

    Added web api sample

---
+++ Amalay.MvcApplications/Amalay.AppStoreWebApi/AppStore/ContextServices/AppStoreContext.cs
@@ -0,0 +1,24 @@
+﻿using AppStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AppStore.ContextServices
+{
+    public class AppStoreContext : DbContext, IAppStoreContext
+    {      
+    
+        public AppStoreContext() : base("name=AppStoreContext")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public void MarkAsModified(Product item)
+        {
+            Entry(item).State = EntityState.Modified;
+        }
+    }
+}
---
+++ Amalay.MvcApplications/Amalay.AppStoreWebApi/AppStore/ContextServices/IAppStoreContext.cs
@@ -0,0 +1,19 @@
+﻿using AppStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppStore.ContextServices
+{
+    public interface IAppStoreContext : IDisposable
+    {
+        DbSet<Product> Products { get; }
+
+        int SaveChanges();
+
+        void MarkAsModified(Product item);
+    }
+}
