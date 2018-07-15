861e678b5cc3f3a8c9b2e67a327368967adbfe8d
Amalay <amalayverma@gmail.com>
2018-07-15 16:59:41 +0530

    Added SOLID principles sample

---
+++ Amalay.WindowApplications/Amalay.WindowApp/Amalay.WindowApp/SOLID/2_O_OpenClosedPrinciple.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amalay.WindowApp.SOLID.OCP
+{
+    //Understanding “O” - Open closed principle
+
+    //Let’s continue with our same customer class example. I have added a simple customer type property to the class. This property decided if this is a “Gold” ora “Silver” customer.
+    //Depending on the same it calculates discount.Have a look at the “getDiscount” function which returns discount accordingly. 1 for Gold customer and 2 for Silver customer.
+    //Guess, what’s the problem with the below code
+
+    public class OpenClosedPrinciple
+    {
+
+    }
+
+    public class Customer
+    {
+        private int _CustType;
+
+        public int CustType
+        {
+            get { return _CustType; }
+            set { _CustType = value; }
+        }
+
+        public double getDiscount(double TotalSales)
+        {
+            if (_CustType == 1)
+            {
+                return TotalSales - 100;
+            }
+            else
+            {
+                return TotalSales - 50;
+            }
+        }
+    }
+
+    //The problem is if we add a new customer type we need to go and add one more “IF” condition in the “getDiscount” function, in other words we need to change the customer class.
+    //If we are changing the customer class again and again, we need to ensure that the previous conditions with new one’s are tested again, existing client’s which are referencing 
+    //this class are working properly as before.
+    //In other words we are “MODIFYING” the current customer code for every change and every time we modify we need to ensure that all the previous functionalities and connected client are 
+    //working as before.
+
+    //How about rather than “MODIFYING” we go for “EXTENSION”. In other words every time a new customer type needs to be added we create a new class as shown in the below.
+    //So whatever is the current code they are untouched and we just need to test and check the new classes.
+
+
+    //=========================================================
+    public class Customer_1
+    {
+        public virtual double GetDiscount(double TotalSales)
+        {
+            return TotalSales;
+        }
+
+        public virtual void AddCustomer()
+        {
+            //Method to add particular customer to the respected tables inside database.
+        }
+    }
+
+    public class SilverCustomer : Customer_1
+    {
+        public override double GetDiscount(double TotalSales)
+        {
+            return base.GetDiscount(TotalSales) - 50;
+        }
+
+        public override void AddCustomer()
+        {
+            //Method to add silver customer to the silver customer table inside database.
+        }
+    }
+
+    public class GoldCustomer : Customer_1
+    {
+        public override double GetDiscount(double TotalSales)
+        {
+            return base.GetDiscount(TotalSales) - 100;
+        }
+
+        public override void AddCustomer()
+        {
+            //Method to add gold customer to the gold customer table inside database.
+        }
+    }
+
+    //Putting in simple words the “Customer” class is now closed for any new modification but it’s open for extensions when new customer types are added to the project.
+}
---
+++ Amalay.WindowApplications/Amalay.WindowApp/Amalay.WindowApp/SOLID/4_I_InterfaceSegregationPrinciple.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amalay.WindowApp.SOLID.ISP
+{
+    //Understanding “I” - ISP (Interface Segregation principle)
+    //Now assume that our customer class has become a SUPER HIT component and it’s consumed across 1000 clients and they are very happy using the customer class.
+
+    //Now let’s say some new clients come up with a demand saying that we also want a method which will help us to “Read” customer data. 
+
+    public class InterfaceSegregationPrinciple
+    {
+
+    }
+
+    public class FileLogger
+    {
+        public void Handle(string error)
+        {
+            System.IO.File.WriteAllText(@"c:\Error.txt", error);
+        }
+    }
+
+    public interface IDiscount
+    {
+        double GetDiscount(double TotalSales);
+    }
+
+    public interface IDatabase
+    {
+        void AddCustomer();
+
+        //void Read();
+    }
+
+    //So developers who are highly enthusiastic would like to change the “IDatabase” interfaceas and add Read() method shown above.
+    //But by doing so we have done something terrible, can you guess ? 
+    //If you visualize the new requirement which has come up, you have two kinds of client’s: -
+    //1. Who want’s just use “Add” method.
+    //2. The other who wants to use “Add” + “Read”.
+
+    //Now by changing the current interface you are doing an awful thing, disturbing the 1000 satisfied current client’s , even when they are not interested in the “Read” method. 
+    //You are forcing them to use the “Read” method.
+    
+    public class SilverCustomer : IDiscount, IDatabase
+    {
+        private FileLogger obj = new FileLogger();
+
+        public double GetDiscount(double TotalSales)
+        {
+            return TotalSales - 50;
+        }
+
+        public void AddCustomer()
+        {
+            try
+            {
+                // Database code goes here
+            }
+            catch (Exception ex)
+            {
+                obj.Handle(ex.Message.ToString());
+            }
+        }
+    }
+
+    public class GoldCustomer : IDiscount, IDatabase
+    {
+        private FileLogger obj = new FileLogger();
+
+        public double GetDiscount(double TotalSales)
+        {
+            return TotalSales - 100;
+        }
+
+        public void AddCustomer()
+        {
+            try
+            {
+                // Database code goes here
+            }
+            catch (Exception ex)
+            {
+                obj.Handle(ex.Message.ToString());
+            }
+        }
+    }
+
+    public class DimondCustomer : IDiscount, IDatabase
+    {
+        private FileLogger obj = new FileLogger();
+
+        public double GetDiscount(double TotalSales)
+        {
+            return TotalSales - 100;
+        }
+
+        public void AddCustomer()
+        {
+            try
+            {
+                // Database code goes here
+            }
+            catch (Exception ex)
+            {
+                obj.Handle(ex.Message.ToString());
+            }
+        }
+
+        public void Read()
+        {
+            // Implements  logic for read
+        }
+    }
+
+    //==============================================================================
+    //So a better approach would be to keep existing clients in their own sweet world and the serve the new client’s separately.
+    //So the better solution would be to create a new interface rather than updating the current interface. So we can keep the current interface “IDatabase” as it is and 
+    //add a new interface “IDatabaseV1” with the “Read” method the “V1” stands for version 1.
+
+    public interface IDatabaseV1 : IDatabase // Gets the Add method
+    {
+        void Read();
+    }
+
+    public class SilverCustomer_1 : IDiscount, IDatabase
+    {
+        private FileLogger obj = new FileLogger();
+
+        public double GetDiscount(double TotalSales)
+        {
+            return TotalSales - 50;
+        }
+
+        public void AddCustomer()
+        {
+            try
+            {
+                // Database code goes here
+            }
+            catch (Exception ex)
+            {
+                obj.Handle(ex.Message.ToString());
+            }
+        }
+    }
+
+    public class GoldCustomer_1 : IDiscount, IDatabase
+    {
+        private FileLogger obj = new FileLogger();
+
+        public double GetDiscount(double TotalSales)
+        {
+            return TotalSales - 100;
+        }
+
+        public void AddCustomer()
+        {
+            try
+            {
+                // Database code goes here
+            }
+            catch (Exception ex)
+            {
+                obj.Handle(ex.Message.ToString());
+            }
+        }
+    }
+
+    public class DimondCustomer_1 : IDiscount, IDatabaseV1
+    {
+        private FileLogger obj = new FileLogger();
+
+        public double GetDiscount(double TotalSales)
+        {
+            return TotalSales - 100;
+        }
+
+        public void AddCustomer()
+        {
+            try
+            {
+                // Database code goes here
+            }
+            catch (Exception ex)
+            {
+                obj.Handle(ex.Message.ToString());
+            }
+        }
+        public void Read()
+        {
+            // Implements  logic for read
+        }
+    }   
+}
