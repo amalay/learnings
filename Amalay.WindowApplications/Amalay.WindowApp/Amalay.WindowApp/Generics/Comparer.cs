a04bed38ed4a750e127deb2f6e2b9034ce6de377
Amalay <amalayverma@gmail.com>
2018-07-21 15:45:12 +0530

    added generic comparer

---
+++ Amalay.WindowApplications/Amalay.WindowApp/Amalay.WindowApp/Generics/Comparer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amalay.WindowApp
+{
+    public class Comparer<Unknown>
+    {
+        #region "Singleton Intance"
+
+        private static readonly Comparer<Unknown> _Instance = new Comparer<Unknown>();
+
+        private Comparer()
+        {
+
+        }
+
+        public static Comparer<Unknown> Instance
+        {
+            get
+            {
+                return _Instance;
+            }
+        }
+
+        #endregion
+
+        private bool Compare(Unknown t1, Unknown t2)
+        {
+            if (t1.Equals(t2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void ComparerDemo()
+        {
+            Comparer<int> oComparerInt = new Comparer<int>();
+            var result = oComparerInt.Compare(10, 10);
+
+            Comparer<string> oComparerStr = new Comparer<string>();
+            var result1 = oComparerStr.Compare("jdhsjhds", "10");
+        }
+    }
+}
