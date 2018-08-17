54518fba941a886fdb2c24019f2a2f720ef687f2
Amalay <amalayverma@gmail.com>
2018-08-17 14:05:35 +0530

    added service fabric sample

---
+++ ServiceFabricApplications/MailServerApplication/MailClient/Program.cs
@@ -0,0 +1,120 @@
+﻿using MailServer.Contracts.Contracts;
+using MailServer.Contracts.Model;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailClient
+{
+    class Program
+    {
+        private static readonly object _lock = new object();
+
+        static void Main(string[] args)
+        {
+            IMailService service = GetServiceProxy();
+
+            Task.Run(() =>
+            {
+                for (;;)
+                {
+                    GetMailCount(service);
+                    Task.Delay(500).Wait();
+                }
+            });
+
+            Task.Run(() =>
+            {
+                for(;;)
+                {
+                    SendMail(service);
+                    Task.Delay(500).Wait();
+                }
+            });
+
+            Console.Read();
+        }
+               
+        public static void SendMail(IMailService svc)
+        {
+            MailMessage message = new MailMessage
+            {
+                FromId = Guid.NewGuid(),
+                ToId = Guid.NewGuid(),
+                Message = "Hello from ABC"
+            };
+            
+            //send mail
+            svc.SendMessage(message).GetAwaiter().GetResult();
+
+            lock (_lock)
+            {
+                Console.SetCursorPosition(27, 4);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(message.FromId + "                                                                                                                            ");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        public static void GetMailCount(IMailService svc)
+        {
+            //get count
+            long count = svc.GetMessageCount().Result;
+
+            lock (_lock)
+            {
+                Console.SetCursorPosition(27, 7);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(count + "                                                                                                          ");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        public static IMailService GetServiceProxy()
+        {
+            IMailService service = null;
+            string serviceConnection = "";
+
+            if (DisplayPrompt() == 1)
+            {
+                serviceConnection = "fabric:/MailServerApplication/MailServerStatelessService";
+                service = ServiceProxy.Create<IMailService>
+                    (new Uri(serviceConnection));
+            }
+            else
+            {
+                serviceConnection = "fabric:/MailServerApplication/MailServerStatefulService";
+                service = ServiceProxy.Create<IMailService>
+                    (new Uri(serviceConnection), 
+                    new ServicePartitionKey(1), 
+                    Microsoft.ServiceFabric.Services.Communication.Client.TargetReplicaSelector.Default);
+            }
+
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine(" Connected to cluster service :" + serviceConnection);
+            Console.SetCursorPosition(0, 4);
+            Console.Write("   Sending Mail To :");
+
+            Console.SetCursorPosition(0, 7);
+            Console.Write("   Total Mails Processed :");
+
+            return service;
+        }
+
+        public static int DisplayPrompt()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Connect client application to ...");
+            Console.WriteLine();
+            Console.WriteLine("                1. Stateless Mail Service");
+            Console.WriteLine("                2. Stateful Mail Service");
+
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ ServiceFabricApplications/MailServerApplication/MailServerStatefulService/MailServerStatefulService.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Data.Collections;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
+using MailServer.Contracts.Contracts;
+using MailServer.Contracts.Model;
+
+namespace MailServerStatefulService
+{
+    /// <summary>
+    /// An instance of this class is created for each service replica by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class MailServerStatefulService : StatefulService, IMailService
+    {
+        public MailServerStatefulService(StatefulServiceContext context)
+            : base(context)
+        { }
+        
+        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
+        {
+            return new[]
+            {
+                new ServiceReplicaListener(c => this.CreateServiceRemotingListener(c))
+            };
+        }
+
+        /// <summary>
+        /// This is the main entry point for your service replica.
+        /// This method executes when this replica of your service becomes primary and has write status.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            var inputMailQueue = await this.StateManager.GetOrAddAsync<IReliableQueue<MailMessage>>("inputMailQueue");
+            var mailDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<Guid, string>>("mailDictionary");
+
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                using (var tx = this.StateManager.CreateTransaction())
+                {
+                    //dequeue mail
+                    var mail = await inputMailQueue.TryDequeueAsync(tx);
+
+                    if (mail.HasValue)
+                    {
+                        var processedMail = mail.Value;
+                        processedMail.ReceivedAt = DateTime.Now;
+
+                        //process and add to state
+                        await mailDictionary.AddAsync(tx, processedMail.FromId, processedMail.Message);
+                    }
+
+                    // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
+                    // discarded, and nothing is saved to the secondary replicas.
+                    await tx.CommitAsync();
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
+
+
+        #region Mail Service Implementation
+        public async Task<long> GetMessageCount()
+        {
+            var mailDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<Guid, string>>("mailDictionary");
+
+            using (var tx = this.StateManager.CreateTransaction())
+            {
+                return await mailDictionary.GetCountAsync(tx);
+            }
+        }
+
+        public async Task SendMessage(MailMessage message)
+        {
+            using (var tx = this.StateManager.CreateTransaction())
+            {
+                var inputMailQueue = await this.StateManager.GetOrAddAsync<IReliableQueue<MailMessage>>("inputMailQueue");
+
+                await inputMailQueue.EnqueueAsync(tx, message);
+
+                await tx.CommitAsync();
+            }
+        }
+
+        public Task<MailMessage> ReceiveMessage(Guid toId)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
