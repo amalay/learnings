24bffa22afd782de10c20e3a7e5e617ca55e7c56
Amalay Verma <amalayverma@gmail.com>
2018-09-30 23:56:29 +0530

    Added service fabric mesh samples
    Added service fabric mesh samples

---
+++ ServiceFabricMesh/ToDoListApp/ToDoService/Controllers/ToDoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        // GET api/todo
+        [HttpGet]
+        public IEnumerable<Entities.ToDoItem> Get()
+        {
+            return DataContext.ToDoList.Items;
+        }
+
+        // GET api/todo/5
+        [HttpGet("{index}")]
+        public Entities.ToDoItem Get(int index)
+        {
+            return DataContext.ToDoList.Items.ElementAt(index);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{index}")]
+        public void Delete(int index)
+        {
+        }
+    }
+}
---
+++ ServiceFabricMesh/ToDoListApp/WebApp/Controllers/ToDoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private static string backendDNSName = $"{Environment.GetEnvironmentVariable("ToDoServiceName")}";
+        private static Uri backendUrl = new Uri($"http://{backendDNSName}:{Environment.GetEnvironmentVariable("ApiHostPort")}/api/todo");
+
+        public IActionResult Index()
+        {
+            Entities.ToDoItem[] items = null;
+
+            HttpClient client = new HttpClient();
+
+            using (HttpResponseMessage response = client.GetAsync(backendUrl).GetAwaiter().GetResult())
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    items = Newtonsoft.Json.JsonConvert.DeserializeObject<Entities.ToDoItem[]>(response.Content.ReadAsStringAsync().Result);
+                }
+            }
+
+            return View("Index", items);
+        }
+    }
+}
