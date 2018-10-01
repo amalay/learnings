8eaf13149fc8c2afd22517664d0840c7e139090d
Amalay Verma <amalayverma@gmail.com>
2018-10-01 19:01:19 +0530

    updated with missing files
    updated with missing files

---
+++ ServiceFabricMesh/ToDoListApp/ToDoService/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities;
+
+namespace ToDoService
+{
+    public class DataContext
+    {
+        public static Entities.ToDoList ToDoList { get; } = new Entities.ToDoList("Azure learning List");
+
+        static DataContext()
+        {
+            // Seed to-do list
+            ToDoList.Add(Entities.ToDoItem.Load("Learn about microservices", 0, true));
+
+            ToDoList.Add(Entities.ToDoItem.Load("Learn about Service Fabric", 1, true));
+
+            ToDoList.Add(Entities.ToDoItem.Load("Learn about Service Fabric Mesh", 2, false));
+        }
+    }
+}
---
+++ ServiceFabricMesh/ToDoListApp/WebApp/Models/IndexModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    //public class IndexModel
+    //{
+    //    private static string backendDNSName = $"{Environment.GetEnvironmentVariable("ToDoServiceName")}";
+    //    private static Uri backendUrl = new Uri($"http://{backendDNSName}:{Environment.GetEnvironmentVariable("ApiHostPort")}/api/todo");
+
+    //    //public Entities.ToDoItem[] Items = new Entities.ToDoItem[] { };
+
+    //    public Entities.ToDoItem[] OnGet()
+    //    {
+    //        Entities.ToDoItem[] items = null;
+
+    //        HttpClient client = new HttpClient();
+
+    //        using (HttpResponseMessage response = client.GetAsync(backendUrl).GetAwaiter().GetResult())
+    //        {
+    //            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+    //            {
+    //                items = Newtonsoft.Json.JsonConvert.DeserializeObject<Entities.ToDoItem[]>(response.Content.ReadAsStringAsync().Result);
+    //            }
+    //        }
+
+    //        return items;
+    //    }
+    //}
+}
